edbfbb6c316cd390f49f8577ced0f4cd211e277c
Tan Xinhao <97723528+XinhaoTheo@users.noreply.github.com>
2024-12-16 11:33:32 +0800

    Update telegram_bot.py
    change the API key  to avoid attack

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,7 +1,7 @@
 import requests
 
-TELEGRAM_API_TOKEN = "7710579435:AAF0lWMbc1UAWMIlrpNZxwVZeVyFsBC3a_4" 
-TELEGRAM_CHAT_ID = "5430804464"
+TELEGRAM_API_TOKEN = "############" 
+TELEGRAM_CHAT_ID = "###########"
 TELEGRAM_API_URL = f"https://api.telegram.org/bot{TELEGRAM_API_TOKEN}/sendMessage"
 
 def send_telegram_alert(message): 
